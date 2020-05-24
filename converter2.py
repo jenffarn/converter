71780ef925888f4f37b0c732cf2ee723dd20aa1f
jenffarn <jenfarn@hotmail.com>
2020-05-24 13:54:06 +0800

    added converter program to local repository

---
+++ converter2.py
@@ -0,0 +1,14 @@
+def temp_convert(celsius):
+    fahrenheit = (celsius * 9/5) + 32
+    return fahrenheit
+
+temp_c = input("What is the temperature in degrees Celsius? ")
+temp_f = temp_convert(float(temp_c))
+
+if float(temp_c) > 30:
+   print("It is really hot!")
+
+print(temp_c + " degrees Celsius are " + str(temp_f) + " degrees Fahrenheit.")    
+
+
+
